00e990c0816e1a54d321fc7fa6e02de86bbcc5b9
Mario <marioeduardo.garciasalazar@gmail.com>
2023-09-25 21:17:16 -0600

    Correcciones de errores de deploy 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/https://avincer-coder.github.io/app_clima'
+  base:'https://avincer-coder.github.io/app_clima/',
 })
